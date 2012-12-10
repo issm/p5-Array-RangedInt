a9a7052bafd25ccddeb2220ca0ac740985d8ba15
issm <issmxx@gmail.com>
2012-12-10 21:34:34 +0900

    initial commit.

---
+++ lib/Array/RangedInt.pm
@@ -0,0 +1,111 @@
+package Array::RangedInt;
+
+use 5.008;
+use strict;
+use warnings FATAL => 'all';
+
+=head1 NAME
+
+Array::RangedInt - The great new Array::RangedInt!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Array::RangedInt;
+
+    my $foo = Array::RangedInt->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+IWATA, Susumu, C<< <issmxx at gmail.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-array-rangedint at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Array-RangedInt>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Array::RangedInt
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Array-RangedInt>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Array-RangedInt>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Array-RangedInt>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Array-RangedInt/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2012 IWATA, Susumu.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See L<http://dev.perl.org/licenses/> for more information.
+
+
+=cut
+
+1; # End of Array::RangedInt
